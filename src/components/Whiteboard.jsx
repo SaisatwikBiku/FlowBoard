dd68d6049a4e26990d33ae73b74f08f7dbb7ee93
Sai Satwik Bikumandla <sbikumandla@albany.edu>
2026-01-28 18:52:43 -0500

    add dynamic cursor support based on selected tool in Whiteboard component

--- src/components/Whiteboard.jsx
+++ src/components/Whiteboard.jsx
@@ -93,6 +93,19 @@ export default function Whiteboard({ tool, color, lang, brushSize }) {
   const stageWidth = window.innerWidth;
   const stageHeight = window.innerHeight - 100;
 
+  const getCursor = () => {
+    switch (tool) {
+      case "pen":
+        return "url('data:image/svg+xml;utf8,<svg xmlns=\"http://www.w3.org/2000/svg\" width=\"20\" height=\"20\" viewBox=\"0 0 20 20\"><circle cx=\"10\" cy=\"10\" r=\"3\" fill=\"black\"/></svg>') 10 10, auto";
+      case "eraser":
+        return "url('data:image/svg+xml;utf8,<svg xmlns=\"http://www.w3.org/2000/svg\" width=\"20\" height=\"20\" viewBox=\"0 0 20 20\"><rect x=\"4\" y=\"4\" width=\"12\" height=\"12\" fill=\"black\" stroke=\"black\"/></svg>') 10 10, auto";
+      case "sticky":
+        return "url('data:image/svg+xml;utf8,<svg xmlns=\"http://www.w3.org/2000/svg\" width=\"20\" height=\"20\" viewBox=\"0 0 20 20\"><rect x=\"3\" y=\"3\" width=\"14\" height=\"14\" fill=\"%23ffeb3b\" stroke=\"black\"/></svg>') 10 10, auto";
+      default:
+        return "default";
+    }
+  };
+
   return (
     <div className="board-wrapper">
       <div className="board-top-controls">
@@ -111,6 +124,7 @@ export default function Whiteboard({ tool, color, lang, brushSize }) {
         onMouseMove={handleMouseMove}
         onMouseUp={handleMouseUp}
         className="stage"
+        style={{ cursor: getCursor() }}
       >
         <Layer>
           {lines.map((line, i) => (
